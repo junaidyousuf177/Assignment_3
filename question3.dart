89dc6cd4d8a83aa1844733206750120b7cf7dd54
junaidyousuf177 <junaidyousuf177@gmail.com>
2025-11-01 06:07:48 +0500

    Questiion 3

---
+++ question3.dart
@@ -0,0 +1,8 @@
+void main (){
+    List student = ['Junaid', '16', '1497', 'A','80'];
+    print("Name:${student[0]}");
+    print("Class:${student[1]}");
+    print("Roll No: ${student[2]}");
+    print("Grade: ${student[3]}");
+    print("Percentage: ${student[4]}%");
+}
